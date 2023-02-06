3ffc3a4611a3499caaed7f531ab2df62474d5dd1
Theanh0411 <theanh@mogul.sg>
2023-02-06 22:25:57 +0700

    feat: add 2 api create and valid access code

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,17 +1,23 @@
 import express from "express";
 import firebase from "firebase-admin";
-import { Request, Response } from 'express';
+import { getFirestore, Timestamp, FieldValue } from 'firebase-admin/firestore'
 
 
 // import  serviceAccount from "../firebase-config.json";
-var serviceAccount = require("../firebase-config.json");
+const serviceAccount = require("../firebase-config.json");
 // console.log(serviceAccount);
 
 const router = express.Router();
 firebase.initializeApp({
   credential: firebase.credential.cert(serviceAccount),
+  databaseURL: "https://skipli-7fdaf.asia-southeast1.firebaseio.com",
+
 });
-router.post("/login", async (req:Request, res: Response) => {
+
+const db = getFirestore();
+
+
+router.post("/login", async (req, res) => {
     const { email, password } = req.body;
     
     try {
@@ -40,6 +46,45 @@ router.post("/login", async (req:Request, res: Response) => {
       res.status(201).send({ token });
     } catch (error) {
       res.status(400).send({ error });
+    }
+  });
+
+  router.post("/save-phone-number", async (req, res) => {
+    const { phoneNumber } = req.body;
+    const accessCode = Math.floor(100000 + Math.random() * 900000);
+  
+    try {
+        const docRef = db.collection('users').doc(phoneNumber);
+        await docRef.set({
+            accessCode
+        })
+      res.status(201).send({ message: "Phone number and access code saved." });
+    } catch (error) {
+      res.status(400).send({ error });
+    }
+  });
+
+  router.post("/valid-access-code", async (req, res) => {
+    const { phoneNumber, accessCode } = req.body;
+  
+    try {
+        console.log("hello");
+        
+        const userRef  = db.collection(`users`).doc(phoneNumber)
+        const doc = await userRef.get();
+        if (!doc.exists) {
+            res.status(201).send({ message: "Can not find this phone number." });
+        } else {
+            const data = doc.data()
+            if(data?.accessCode === accessCode) {
+                res.status(200).send({ message: "Successfully" });
+            }
+            else {
+                res.status(200).send({ message: "Invalid access code" });
+            }
+        }
+    } catch (error) {
+      res.status(400).send({ error: error });
     }
   });
 
