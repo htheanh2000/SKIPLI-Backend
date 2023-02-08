06ba8aa788154db05cd5cae67417ea00e923d05e
Theanh0411 <theanh@mogul.sg>
2023-02-08 15:56:21 +0700

    feat: set up SMS service

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import firebase from "firebase-admin";
-import { getFirestore, Timestamp, FieldValue } from 'firebase-admin/firestore'
-
+import { getFirestore } from 'firebase-admin/firestore'
+const accountSid = "ACb1c63a6683608849e2c2a5cc65524a22";
+const authToken = process.env.TWILIO_AUTH_TOKEN || '937a9b000e7966bb0ea544c5775946e8';
+console.log(authToken);
+const client = require("twilio")(accountSid, authToken);
 
 // import  serviceAccount from "../firebase-config.json";
 const serviceAccount = require("../firebase-config.json");
@@ -60,6 +63,11 @@ router.post("/login", async (req, res) => {
         await docRef.set({
             accessCode
         })
+        // await client.messages
+        // .create({ body: `Your access code is: ${accessCode}`, from: "+17205838785", to: "+84782778712" })
+        // .then((message: { sid: any; }) => console.log(message.sid));
+
+
       res.status(201).send({ message: "Phone number and access code saved." });
     } catch (error) {
       res.status(400).send({ error });
@@ -76,7 +84,8 @@ router.post("/login", async (req, res) => {
             res.status(201).send({ message: "Can not find this phone number." });
         } else {
             const data = doc.data()
-            if(data?.accessCode === accessCode) {
+            // TO DO: validate api and change == to === later 
+            if(data?.accessCode == accessCode) {
                 const docRef = db.collection('users').doc(user);
                 await docRef.set({
                     accessCode: ''
